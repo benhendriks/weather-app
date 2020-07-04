b95ca5e62c444566c580540d1398d18f93d0d3c9
Benjamin Steidl <localartpool@gmail.com>
2020-07-04 14:21:55 +0200

    Worked on api and skycon canvas

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 window.addEventListener("load", () => {
   let long;
   let lat;
-  const temperatureDescription = document.querySelector(`.temperature-description`);
-  const temperatureDegree = document.querySelector(`.temperature-degree`);
-  const locationTimezone = document.querySelector(`.location-timezone`);
-  const temperatureSection = document.querySelector(`.temperature-section`);
-  const temperatureSpan = document.querySelector(`.temperature-section span`);
+  let temperatureDescription = document.querySelector(`.temperature-description`);
+  let temperatureDegree = document.querySelector(`.temperature-degree`);
+  let locationTimezone = document.querySelector(`.location-timezone`);
+  let temperatureSection = document.querySelector(`.temperature`);
+  let temperatureSpan = document.querySelector(`.temperature span`);
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
-    long = position.coords.lonitude;
+    long = position.coords.longitude;
     lat = position.coords.latitude;
 
-    const api = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={c86d69b9a28eb6e27e8c1d1f8f31612d}`;
+    const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=c86d69b9a28eb6e27e8c1d1f8f31612d&units=imperial`;
 
      fetch(api)
         .then(response => {
           return response.json()
         })
         .then(data => {
-          const { temperature, summary, icon } = data.currently;
+          const { temp, description, icon } = data.main;
           //Set DOM Elements from the API
-          temperatureDegree.textContent = temperature;
-          temperatureDescription.textContent = summary;
-          temperatureDegree.textContent = temperature;
-          locationTimezone = data.timezone;
+          temperatureDegree.textContent = temp;
+          temperatureDescription.textContent = data.weather[0].description;
+          temperatureDegree.textContent = temp;
+          locationTimezone = data.sys.country;
             //Formula for Celsius
-            let celsius = (temperature - 32) * (5 / 9);
+            let celsius = (temp - 32) * (5 / 9);
             //Set Icon
             setIcons(icon, document.querySelector(".icon"));
 
@@ -37,7 +37,7 @@ window.addEventListener("load", () => {
                 temperatureDegree.textContent = Math.floor(celsius);
               }else{
                 temperatureSpan.textContent = "F";
-                temperatureDegree.textContent = temperature;
+                temperatureDegree.textContent = temp;
               }
             });
         });
